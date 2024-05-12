f8915a7c3aa91e4abab53b73541d6cff6ea10d2e
edwinparrales <edwinparrales82@gmail.com>
2024-05-11 19:42:49 -0500

    inicializando el proyecto

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import ejs from 'ejs';
 import {dirname,join} from 'path';
 import {fileURLToPath} from 'url';
-import router from './routes/routes.js';
+import router from './src/routes/routes.js';
 const app = express();
 const port = process.env.PORT || 3000;
 const __dirname =  dirname(fileURLToPath(import.meta.url));
