e64eb1700e1b0071f5ebede28e1e10a8909c1c9e
edwinparrales <edwinparrales82@gmail.com>
2024-05-12 00:03:49 -0500

    inicializando el proyecto

--- src/routes/routes.js
+++
@@ -1,8 +0,0 @@
-import {Router} from 'express';
-const router = Router();
-router.get('/', (req, res) => {
-    res.render('index',{title: 'Sistema-Epv'});
-});
-router.get('/about', (req, res) => { res.render('about',{title: 'Epv-Sobre nosotros'}); });
-router.get('/contact', (req, res) => { res.render('contact',{title: 'Epv-Contacto'}); });
-export default router;
