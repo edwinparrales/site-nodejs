47993c15880d5bbed01f30b44e3637e3c8ba926d
edwinparrales <edwinparrales82@gmail.com>
2024-05-11 19:59:15 -0500

    inicializando el proyecto

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const port = process.env.PORT || 3000;
 const __dirname =  dirname(fileURLToPath(import.meta.url));
 
 
-app.set('views', join(__dirname, 'views'));
+app.set('views', join(__dirname, 'src/views'));
 app.set('view engine', 'ejs');
 app.use(router);
-app.use(express.static(join(__dirname,'public')));
+app.use(express.static(join(__dirname,'src/public')));
 
 
 app.listen(port);
