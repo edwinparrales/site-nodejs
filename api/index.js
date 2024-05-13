6efd26a5a49a81caa14956b2e3b7c2b0865195d9
edwinparrales <edwinparrales82@gmail.com>
2024-05-12 19:49:57 -0500

    inicializando el proyecto

---
+++ api/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import ejs from 'ejs';
+import {dirname,join} from 'path';
+import {fileURLToPath} from 'url';
+import router from "./routes/routes.js";
+const app = express();
+const port = process.env.PORT || 3000;
+const __dirname =  dirname(fileURLToPath(import.meta.url));
+
+
+app.set('views', join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+app.use(router);
+app.use(express.static(join(__dirname,'/public')));
+
+
+app.listen(port);
+
